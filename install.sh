f3a2da3284a7d331ed7ee330e9d4848a798da257
Anders Roxell <anders.roxell@linaro.org>
2016-11-20 20:00:26 +0100

    bash|zsh: source shell dir
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@
 reporoot=$(readlink -f $(dirname $0))
 
 install_files=(
+    "$HOME/.shell:${reporoot}/shell"
+    "$HOME/.zsh:${reporoot}/zsh"
     "$HOME/.zshrc:${reporoot}/zsh/zshrc"
     "$HOME/.zlogin:${reporoot}/zsh/zlogin"
     "$HOME/.dir_colors:${reporoot}/dir_colors"
+    "$HOME/.bash:${reporoot}/bash"
     "$HOME/.bashrc:${reporoot}/bash/bashrc"
     "$HOME/.bash_profile:${reporoot}/bash/bash_profile"
     "$HOME/.bash_logout:${reporoot}/bash/bash_logout"
